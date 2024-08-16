7b3737894d590106705f48efe94cbb5294d830a6
Kingvics <okoromivictorsunday@gmail.com>
2024-08-16 13:39:49 +0100

    changed to aws

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://rppserver.onrender.com/api/v1';
+const BASE_URL = 'https://rppserver.nmtsconsulting.com/api/v1';
 
 export default axios.create({
   baseURL: BASE_URL,
