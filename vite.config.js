06dd227eca80a45b150d657d6e549d524d2b9de4
Kingvics <okoromivictorsunday@gmail.com>
2025-04-27 23:20:24 +0100

    updated17

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         theme_color: '#007bff',
         icons: [
           {
-            src: '/public/web1.png',
+            src: '/web1.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/public/web2.png',
+            src: '/web2.png',
             sizes: '512x512',
             type: 'image/png',
           },
