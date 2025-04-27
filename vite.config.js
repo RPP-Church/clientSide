b4f88f930558c04ec0f1859b246db24594f8a4b8
Kingvics <okoromivictorsunday@gmail.com>
2025-04-27 22:58:19 +0100

    updated10

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,12 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'RPP App', // Name of the app
-        short_name: 'RPP', // Short name of the app
-        description: 'A church app for attendance and services', // App description
-        start_url: '/', // The starting point of the app
-        display: 'standalone', // Standalone mode (no browser UI)
-        background_color: '#ffffff', // Background color of the splash screen
-        theme_color: '#000000',
+        name: 'Church Attendance Scanner',
+        short_name: 'Scanner',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#007bff',
         icons: [
           {
             src: '/web1.png',
@@ -26,31 +25,11 @@ export default defineConfig({
             src: '/web2.png',
             sizes: '512x512',
             type: 'image/png',
-          
           },
         ],
-        // screenshots: [
-        //   {
-        //     src: '/src/assets/web4.jpg',
-        //     sizes: '1280x800',
-        //     type: 'image/png',
-        //     form_factor: 'wide',
-        //   },
-        //   {
-        //     src: '/src/assets/web3.jpg',
-        //     sizes: '360x640',
-        //     type: 'image/png',
-        //   },
-        // ],
-      },
-      workbox: {
-        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // Optional: Set file size limit for caching
       },
     }),
   ],
-  build: {
-    outDir: 'dist',
-  },
   server: {
     watch: {
       usePolling: true,
