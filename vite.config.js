a1e28164044ad8368913885dbb7d31c60ed8002e
Kingvics <okoromivictorsunday@gmail.com>
2025-04-27 23:37:25 +0100

    updated20

--- vite.config.js
+++ vite.config.js
@@ -6,32 +6,32 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   manifest: {
-    //     name: 'Church Attendance Scanner',
-    //     short_name: 'Scanner',
-    //     start_url: '/',
-    //     display: 'standalone',
-    //     background_color: '#ffffff',
-    //     theme_color: '#007bff',
-    //     icons: [
-    //       {
-    //         src: '/web1.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/web2.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    //   workbox: {
-    //     maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // Optional: Set file size limit for caching
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Church Attendance Scanner',
+        short_name: 'Scanner',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#007bff',
+        icons: [
+          {
+            src: '/src/assets/web1.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/src/assets/web2.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // Optional: Set file size limit for caching
+      },
+    }),
   ],
   server: {
     watch: {
