abfc96e9198575dcfae73bb18464803e54ef4114
Kingvics <okoromivictorsunday@gmail.com>
2025-04-27 23:44:24 +0100

    updated21

--- vite.config.js
+++ vite.config.js
@@ -15,18 +15,18 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#007bff',
-        icons: [
-          {
-            src: '/src/assets/web1.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/src/assets/web2.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+        // icons: [
+        //   {
+        //     src: '/src/assets/web1.png',
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: '/src/assets/web2.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //   },
+        // ],
       },
       workbox: {
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // Optional: Set file size limit for caching
